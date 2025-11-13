c6ec1a37ad7b098e4930f6f9f6266a00beb5f689
mojadin12345 <mmore3369@gmail.com>
2025-11-13 10:31:33 -0500

    Actualiza paquetes Expo para compatibilidad 53.0.23

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    Platform: "com.moreweb.comida-rapida",
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    databaseId: "691530920029ba754da9",
+    userCollectionId:'user'
+}
